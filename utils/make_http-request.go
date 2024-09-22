8176750ece573cf2cc3c1550891619ca621bbacb
hemamalinii <hpinkkydoggy@gmail.com>
2024-09-22 23:24:35 +0530

    uwuwuwuwu

--- utils/make_http-request.go
+++ utils/make_http-request.go
@@ -1,8 +1,8 @@
 package utils
 
 import (
-	"encoding/json" // Added json import
-	"fmt"           // Added fmt import
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -30,41 +30,34 @@ func MakeHTTPRequest[T any](fullUrl string, httpMethod string, headers map[strin
 		return responseType, err
 	}
 
-	// Set headers
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
 
-	// Make the HTTP request
 	res, err := client.Do(req)
 	if err != nil {
 		return responseType, err
 	}
 	defer res.Body.Close() // Defer after receiving response
 
-	// Check if response is nil
 	if res == nil {
 		return responseType, fmt.Errorf("error: calling %s returned empty response", u.String())
 	}
 
-	// Read the response body
 	responseData, err := io.ReadAll(res.Body)
 	if err != nil {
 		return responseType, err
 	}
 
-	// Check for non-200 status codes
 	if res.StatusCode != http.StatusOK {
 		return responseType, fmt.Errorf("error: calling %s: \n status: %s \n responseData: %s", u.String(), res.Status, responseData)
 	}
 
-	// Unmarshal the response body into the expected response type
 	var responseObject T
 	err = json.Unmarshal(responseData, &responseObject)
 	if err != nil {
 		return responseType, err
 	}
 
-	// Return the unmarshalled object
 	return responseObject, nil
 }
